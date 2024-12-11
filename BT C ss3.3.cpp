78bbef06d364a58c6184d132c38e5bd34a67bb0a
hoanganh635 <hoanganhnv1997@gmail.com>
2024-12-11 21:38:05 +0700

    Add files via upload

---
+++ BT C ss3.3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float S,C,pi = 3.14;
+    int R;
+    printf("moi ban nhap ban kinh R= %d",R);
+    C = 2*(pi*R);
+    S = pi*(R*R);
+    printf("chu vi hinh tron la: %.2f",C);
+    printf("dien tich hinh tron la: %.2f",S);
+    return 0;
+
+}
